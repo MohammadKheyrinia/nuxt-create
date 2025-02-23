8f37d8c9c4d5d9907c564ef9e287d4370eeb1237
MohammadKheyrinia <mkheirenia11@gmail.com>
2025-02-23 16:11:29 +0330

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+    <title>Home Page</title>
+
+    <body>
+        <div class="container">
+            <button class="button" @click="submit()">Click Me</button>
+        </div>
+    </body>
+</template>
+
+<script setup>
+async function submit() {
+    await $fetch('/api/users', {
+        headers: {
+            "Content-Type": "multipart/form-data",
+        },
+        method: 'POST',
+        body: {
+            firstName: 'dadxdx',
+            lastName: 'dd',
+            email: 'asdsads',
+            password: 'asxxsax'
+        }
+    })
+}
+</script>
